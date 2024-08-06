df05ed9eac1e6facebf43219cd29a7de0aa91f8d
edwardlewisgrant@gmail.com <edwardlewisgrant@gmail.com>
2024-08-06 14:33:16 +0000

    staging

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -11,7 +11,8 @@ renamed as (
     select
         date_date,
         orders_id,
-        pdt_id,
+        pdt_id AS products_id,
+        concat(date_date, "_", pdt_id) AS sales_id,
         revenue,
         quantity
 
